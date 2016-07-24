e5ef4be7f2e0cbcfd11ee6eb2b6089c0264fd370
SwiftiCode <swifticode@203.203-211-151.static.qala.com.sg>
2016-07-24 14:30:01 +0800

    finished

--- Intro10/ViewController.swift
+++ Intro10/ViewController.swift
@@ -8,18 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var myName: UITextField!
+    @IBOutlet weak var myStatus: UILabel!
+    @IBOutlet weak var myLastName: UILabel!
 
+    
+    // MARK: Default Template
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myName.delegate = self
+        
+        myStatus.text = "Total = " + String(nameList.count)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
 
+    // MARK: Action
+    @IBAction func addName(sender: UIButton) {
+        
+        myName.resignFirstResponder()
+        
+        let tempName = myName.text ?? ""
+        
+        if !tempName.isEmpty {
+            
+            nameList += [tempName]
+            myStatus.text = "Name Saved! " + "Total = " + String(nameList.count)
+            
+        } else {
+            
+            myStatus.text = "Cannot Save Empty Name! " + "Total = " + String(nameList.count)
+        }
+
+    }
+    
+    @IBAction func displayLastName(sender: UIButton) {
+        
+        myName.resignFirstResponder()
+        
+        if nameList.count > 0 {
+            
+            let displayName = nameList.last!
+            myLastName.text = "The last name in the list is " + displayName
+            
+        } else {
+            
+            myLastName.text = "The last name in the list is No Name"
+        }
+        
+    }
+    
 
 }
 
---
+++ Intro10/data.swift
@@ -0,0 +1,12 @@
+//
+//  data.swift
+//  Intro10
+//
+//  Created by SwiftiCode on 24/7/16.
+//  Copyright © 2016 SwiftiCode. All rights reserved.
+//
+
+import Foundation
+
+var nameList = [String]()
+
